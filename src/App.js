96b98ee513f826cab5c93607da25b8e7f5e112ba
Khanh Tran <tran_khanh@outlook.com>
2020-06-30 17:44:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,125 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBox from './search';
+import Card from './card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  // the api request function
+  fetchApi(url) {
+
+    fetch(url).then((res) => res.json()).then((data) => {
+      // update state with API data
+      this.setState({
+        movieID: data.id,
+        original_title: data.original_title,
+        tagline: data.tagline,
+        overview: data.overview,
+        homepage: data.homepage,
+        poster: data.poster_path,
+        production: data.production_companies,
+        production_countries: data.production_countries,
+        genre: data.genres,
+        release: data.release_date,
+        vote: data.vote_average,
+        runtime: data.runtime,
+        revenue: data.revenue,
+        backdrop: data.backdrop_path
+
+      })
+    })
+
+    // .catch((err) => console.log('Movie not found!'))
+
+  } // end function
+
+  fetchMovieID(movieID) {
+    let url = `https://api.themoviedb.org/3/movie/${movieID}?&api_key=cfe422613b250f702980a3bbf9e90716`
+    this.fetchApi(url)
+  } // end function
+
+  componentDidMount() {
+    let url = `https://api.themoviedb.org/3/movie/${this.state.movieID}?&api_key=cfe422613b250f702980a3bbf9e90716`
+    this.fetchApi(url)
+
+    //========================= BLOODHOUND ==============================//
+    let suggests = new Bloodhound({
+      datumTokenizer: function (datum) {
+        return Bloodhound.tokenizers.whitespace(datum.value);
+      },
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      remote: {
+        url: 'https://api.themoviedb.org/3/search/movie?query=%QUERY&api_key=cfe422613b250f702980a3bbf9e90716',
+        filter: function (movies) {
+          // Map the remote source JSON array to a JavaScript object array
+          return $.map(movies.results, function (movie) {
+            return {
+              value: movie.original_title, // search original title
+              id: movie.id // get ID of movie simultaniously
+            };
+          });
+        } // end filter
+      } // end remote
+    }); // end new Bloodhound
+
+    suggests.initialize(); // initialise bloodhound suggestion engine
+
+    //========================= END BLOODHOUND ==============================//
+
+    //========================= TYPEAHEAD ==============================//
+    // Instantiate the Typeahead UI
+    $('.typeahead').typeahead({
+      hint: true,
+      highlight: true,
+      minLength: 2
+    }, { source: suggests.ttAdapter() }).on('typeahead:selected', function (obj, datum) {
+      this.fetchMovieID(datum.id)
+    }.bind(this)); // END Instantiate the Typeahead UI
+    //========================= END TYPEAHEAD ==============================//
+
+  } // end component did mount function
+
+  fetchApi(url) {
+
+    fetch(url).then((res) => res.json()).then((data) => {
+      // update state with API data
+      this.setState({
+        movieID: data.id,
+        original_title: data.original_title,
+        tagline: data.tagline,
+        overview: data.overview,
+        homepage: data.homepage,
+        poster: data.poster_path,
+        production: data.production_companies,
+        production_countries: data.production_countries,
+        genre: data.genres,
+        release: data.release_date,
+        vote: data.vote_average,
+        runtime: data.runtime,
+        revenue: data.revenue,
+        backdrop: data.backdrop_path
+
+      })
+    })
+
+    // .catch((err) => console.log('Movie not found!'))
+
+  } // end function
+
+  render() {
+    return (
+      <div>
+        <SearchBox fetchMovieID={this.fetchMovieID.bind(this)} />
+        <Card data={this.state} />
+      </div>
+    )
+  } // END render
 }
 
 export default App;
+
+
+
