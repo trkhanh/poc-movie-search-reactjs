8ee1e5e861b05dd74df2c1f71b95f1d334114639
Kane Tran <tran_khanh@outlook.com>
2025-03-23 13:26:40 +0700

    update

--- src/components/Timer/useTimer.js
+++ src/components/Timer/useTimer.js
@@ -1,11 +1,17 @@
 // useTimer.js
 import { useState, useEffect, useCallback, useRef } from 'react';
 
+// Custom hook to handle timer logic
 const useTimer = (initialTime = 60, onTimerEnd = () => {}) => {
   const [timeLeft, setTimeLeft] = useState(initialTime);
   const [isRunning, setIsRunning] = useState(false);
   const intervalRef = useRef(null);
 
+  // Start the timer
+  // In reactjs, when a function recareted, it can cause child components
+  // that depend on it to re-render unnecessarily. Since `start` is likely passed
+  // down to a component, like Button. Memoizing it would prevent that button from
+  // re-rendering every time the parent updates
   const start = useCallback(() => {
     if (!isRunning && timeLeft > 0) {
       setIsRunning(true);
